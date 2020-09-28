fc8b01521446402207b6d3db3788a8368bcc2bec
Rob Williams <rwilliams@spotify.com>
2020-09-27 23:09:32 -0400

    rendering a tiled map created with Tiled Map Editor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use macroquad::*;
-
-const MAP_WIDTH: f32 = 8.;
-const MAP_HEIGHT: f32 = 7.;
+use macroquad_tiled::*;
 
 fn window_conf() -> Conf {
     Conf {
@@ -13,54 +11,33 @@ fn window_conf() -> Conf {
     }
 }
 
-fn get_map_tile(map: &Vec<usize>, x: usize, y: usize) -> usize {
-    map[x + MAP_WIDTH as usize * y]
-}
-
 #[macroquad::main(window_conf)]
 async fn main() {
-    let textures = vec![
-        load_texture("assets/tile/tiles_00.png").await,
-        load_texture("assets/tile/tiles_01.png").await,
-        load_texture("assets/tile/tiles_02.png").await,
-        load_texture("assets/tile/tiles_03.png").await,
-        load_texture("assets/tile/tiles_04.png").await,
-        load_texture("assets/tile/tiles_05.png").await,
-        load_texture("assets/tile/tiles_06.png").await,
-        load_texture("assets/tile/tiles_07.png").await,
-        load_texture("assets/tile/tiles_08.png").await,
-        load_texture("assets/tile/tiles_09.png").await,
-        load_texture("assets/tile/tiles_10.png").await
-    ];
-    let map: Vec<usize> = vec![
-        1,1,1,1,5,6,7,1,
-        1,1,1,1,5,6,7,1,
-        1,1,1,1,5,6,7,1,
-        3,3,3,3,11,6,7,1,
-        9,9,9,9,9,9,10,1,
-        1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,2,3,
-    ];
-    
 
-    let tile_size = textures[0].width(); // being lazy here since it's a square, both width and height are same
+    let map_json_bytes = load_file("assets/maps/map.json").await.expect("failed to load map.json");
+    let map_json_str = String::from_utf8(map_json_bytes).expect("failed to convert JSON to utf-8");
+    let texture_atlas = load_texture("assets/texture/atlas.png").await;
+    let map = load_map(
+        &map_json_str,
+        &[
+            ("atlas.png", texture_atlas),
+        ],
+    ).expect("failed to load map");
 
-    // TODO: figure this out -- right now this works but leads to an inverted view somehow.
-    // build camera such that we are zoomed in on the 8x7 grid of tiles
-    // set_camera(Camera2D::from_display_rect(Rect::new(0., 0., MAP_WIDTH * tile_size, MAP_HEIGHT * tile_size)));
-    
+    // Lets draw the whole map full screen
+    // Default macroquad camera is pixel perfect with (0, 0) in top left corner and (screen_width(), screen_height()) on bottom right
+    let dest_rect = Rect::new(0., 0., screen_width(), screen_height());
+
+    // We used only part of tiled canvas to create our first level
+    // So lets draw only that part of the canvas
+    // Area is hardcoded for now, but we will use the technique of drawing parts of tiled canvas
+    // to jump through level sections in the future
+    let source_rect = Rect::new(0., 0., 7., 6.);
+
     loop {
         clear_background(BLACK);
-        for x in 0..MAP_WIDTH as usize {
-            for y in 0..MAP_HEIGHT as usize {
-                draw_texture(
-                    textures[get_map_tile(&map, x, y) - 1],
-                    0. + x as f32 * tile_size,
-                    0. + y as f32 * tile_size,
-                    WHITE,
-                );
-            }
-        }
-        next_frame().await
+        map.draw_tiles("Tile Layer 1", dest_rect, source_rect);
+        next_frame().await;
     }
+
 }
