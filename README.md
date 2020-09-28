290af2db7236f17bf76f9694ffdd4903b0212f93
Rob Williams <rwilliams@spotify.com>
2020-09-27 23:22:46 -0400

    use larger_map from RPG example tileset-14

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # rpg-explore
 
 This will be my scratch project where I follow along with the Part 1 (Explore) of the [How to make an RPG book](http://howtomakeanrpg.com/a/how-to-make-an-rpg-release.html). Except I'm doing it in Rust with [macroquad](https://github.com/not-fl3/macroquad) library.
+
+## Using Tiled maps
+
+The RPG book provides a lot of Tiled maps for the [Tiled Map Editor](https://www.mapeditor.org/). However, it uses the LUA export and enables compression. The `macroquad-tiled` subcrate
+(only available in the `0.3` branch, by the way) seems to only support the uncompressed CSV format of tile layer data. Therefore, for each map I want to load from the RPG book, I am needing
+to open it in Tiled Map Editor, change the tile layer format to CSV, and then export as JSON.
--- src/main.rs
+++ src/main.rs
@@ -14,28 +14,21 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() {
 
-    let map_json_bytes = load_file("assets/maps/map.json").await.expect("failed to load map.json");
+    let map_json_bytes = load_file("assets/maps/larger_map.json").await.expect("failed to load larger_map.json");
     let map_json_str = String::from_utf8(map_json_bytes).expect("failed to convert JSON to utf-8");
-    let texture_atlas = load_texture("assets/texture/atlas.png").await;
+    let texture_atlas = load_texture("assets/texture/cave16x16.png").await;
     let map = load_map(
         &map_json_str,
         &[
-            ("atlas.png", texture_atlas),
+            ("cave16x16.png", texture_atlas),
         ],
     ).expect("failed to load map");
 
-    // Lets draw the whole map full screen
-    // Default macroquad camera is pixel perfect with (0, 0) in top left corner and (screen_width(), screen_height()) on bottom right
-    let dest_rect = Rect::new(0., 0., screen_width(), screen_height());
-
-    // We used only part of tiled canvas to create our first level
-    // So lets draw only that part of the canvas
-    // Area is hardcoded for now, but we will use the technique of drawing parts of tiled canvas
-    // to jump through level sections in the future
-    let source_rect = Rect::new(0., 0., 7., 6.);
-
     loop {
         clear_background(BLACK);
+        let dest_rect = Rect::new(0., 0., screen_width(), screen_height());
+        // note: this renders the top-left 16x16 portion of our map
+        let source_rect = Rect::new(0., 0., 15., 15.);
         map.draw_tiles("Tile Layer 1", dest_rect, source_rect);
         next_frame().await;
     }
