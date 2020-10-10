d65b3e8bc4053352e2c6607d904c88f167550324
Rob Williams <rwilliams@spotify.com>
2020-10-10 11:08:01 -0400

    improve wasm deploy

--- scripts/deploy_wasm.sh
+++ scripts/deploy_wasm.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: ./scripts/deploy_wasm.sh <deployment_name>"
+    exit 1
+fi
 
-gsutil -h "Cache-Control:no-cache,max-age=0" cp rpg-explore.html gs://staging.robwil.io/
-gsutil -h "Cache-Control:no-cache,max-age=0" -h "Content-Type: application/wasm" cp rpg-explore.wasm gs://staging.robwil.io/
-gsutil -h "Cache-Control:no-cache,max-age=0" cp -r assets/ gs://staging.robwil.io/
+gsutil -h "Cache-Control:no-cache,max-age=0" cp rpg-explore.html gs://staging.robwil.io/rpg-explore/$1/index.html
+gsutil -h "Cache-Control:no-cache,max-age=0" -h "Content-Type: application/wasm" cp rpg-explore.wasm gs://staging.robwil.io/rpg-explore/$1/
+gsutil -h "Cache-Control:no-cache,max-age=0" cp -r assets/ gs://staging.robwil.io/rpg-explore/$1/
